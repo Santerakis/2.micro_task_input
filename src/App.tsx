e4b77f0963e8578233185367cfc64732d4ccbaed
santerakis <santerakis@gmail.com>
2022-11-25 22:04:29 +0200

    addMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./Components/FullInput";
+
 
 function App() {
-    let[message, setMessage] = useState([
+    let [message, setMessage] = useState([
         {message: 'message1'},
         {message: 'message2'},
         {message: 'message3'},
     ])
+
+    const addMessage = (title: string) => {
+        let newMessage = {massage: 'messageNew'};
+        console.log(title)
+
+    }
     return (
         <div>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            {message.map((el, index)=>{
+            {/*<div>*/}
+            {/*    <input/>*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
+
+            <FullInput addMessage={addMessage}/>
+            {message.map((el, index) => {
                 return (
                     <div key={index}>{el.message}</div>
                 )
